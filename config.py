3efc23a013b33a5748d036ac591ff671e315e216
conversatie <florea.ionut@outlook.com>
2021-08-14 19:01:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 # Pepkobot Copyright © 2018 Il'ya Marshal Semyonov
 # License: https://www.gnu.org/licenses/gpl-3.0.en.html
 import os
-
+import pymysql.cursors
 
 telegram_token = os.environ.get('TELEGRAM_BOT_TOKEN')
 
