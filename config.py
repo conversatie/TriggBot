6e4e3088f1e013f71e950484df2d8eb06f229ff6
conversatie <florea.ionut@outlook.com>
2021-08-14 18:31:10 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,15 +4,24 @@
 # License: https://www.gnu.org/licenses/gpl-3.0.en.html
 import os
 import pymysql.cursors
-
-
 telegram_token = os.environ.get('TELEGRAM_BOT_TOKEN', '1908335294:AAGJDtPL5SyTMQRQonybbB3MlFXyIgkAXbg')
-import pymysql.cursors
 
-# Connect to the database
-DB = pymysql.connect(host='localhost',
-                             user='test',
-                             password='Bucuresti.1',
-                             database='triggers',
-                             cursorclass=pymysql.cursors.DictCursor)
+# Create a mySQL Database
+# Establish connection
+connection = pymysql.connect(host='localhost',
+                             user='root',
+                             port='',
+                             password='')
+ 
+# Simulate the CREATE DATABASE function of mySQL
+try:
+    with connection.cursor() as cursor:
+        cursor.execute('CREATE DATABASE triggers')
+ 
+finally:
+    connection.close()
+     
+    
+finally:
+    connection.close()
 
